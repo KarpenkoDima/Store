20022fc763aefaea518fd8c3634ff88f29929e52
unknown <dimka2010@gmail.com>
2020-10-20 21:30:52 +0300

    Release search by ISBN and Authors or Title

---
+++ StoreMemory/BookRepository.cs
@@ -0,0 +1,30 @@
+﻿using store;
+using Store;
+using System;
+using System.Linq;
+
+namespace StoreMemory
+{
+    public class BookRepository : IBookRepository
+    {
+        readonly Book[] books = new[] 
+        { new Book(1,"ISBN 12345-67891", "Donald Knut", "Art of Programming"),
+            new Book(2, "ISBN 11111-11111", "M. Fowler", "Refactoring"), 
+            new Book(3, "ISBN 09876-54321", "B. Kernigan", "C Programming Language") 
+        };
+
+        public Book[] GetAllByISBN(string isbn)
+        {
+            return books
+                .Where(book => book.ISBN == isbn)
+                .ToArray();
+        }
+
+        public Book[] GetAllByTittleOrAuthor(string titlePart)
+        {
+            return books
+                .Where(book => book.Title.Contains(titlePart) || book.Author.Contains(titlePart))
+                .ToArray();
+        }
+    }
+}
